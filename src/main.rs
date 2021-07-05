280ad48a34e99d9a44032e840795a4b5b5d08ee3
snowmerak <go_dev@kakao.com>
2021-07-05 21:08:52 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,11 @@ pub mod N1903_largest_odd_number;
 pub mod N1904_number_of_rounds;
 pub mod N1905_count_sub_islands;
 pub mod N1906_min_difference;
+pub mod N1512_num_identical_pairs;
 
 fn main() {
-    let rs = Solution::min_difference(vec![4,5,2,2,7,10], vec![vec![2,3],vec![0,2],vec![0,5],vec![3,5]]);
-    println!("{:?}", &rs);
+    let rs = Solution::num_identical_pairs(vec![1, 1, 1, 1]);
+    println!("{}", rs);
 }
 
 struct Solution {}
