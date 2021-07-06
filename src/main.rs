5693a9717a2c1b061f584a3b713e55c028d92ccd
snowmerak <go_dev@kakao.com>
2021-07-06 10:13:12 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,10 @@ pub mod N1904_number_of_rounds;
 pub mod N1905_count_sub_islands;
 pub mod N1906_min_difference;
 pub mod N1512_num_identical_pairs;
+pub mod N1137_tribonacci;
 
 fn main() {
-    let rs = Solution::num_identical_pairs(vec![1, 1, 1, 1]);
+    let rs = Solution::tribonacci(25);
     println!("{}", rs);
 }
 
