bdbbeca46e01420c5216cb76d0b60558197fadd1
snowmerak <go_dev@kakao.com>
2021-07-07 15:12:49 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,11 @@ pub mod N1905_count_sub_islands;
 pub mod N1906_min_difference;
 pub mod N1512_num_identical_pairs;
 pub mod N1137_tribonacci;
+pub mod N1713_min_operations;
 
 fn main() {
-    let rs = Solution::tribonacci(25);
-    println!("{}", rs);
+    let rs = Solution::min_difference(vec![4,5,2,2,7,10], vec![vec![2,3],vec![0,2],vec![0,5],vec![3,5]]);
+    println!("{:?}", rs);
 }
 
 struct Solution {}
