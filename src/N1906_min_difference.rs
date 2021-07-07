cb402dd0b9b6f010b413e41a794da06f33f5e088
snowmerak <go_dev@kakao.com>
2021-07-07 11:52:17 +0900

    Update TIMEOUT!!

--- src/N1906_min_difference.rs
+++ src/N1906_min_difference.rs
@@ -1,37 +1,29 @@
 use crate::Solution;
 
-use std::collections::BTreeSet;
-
 impl Solution {
     pub fn min_difference(nums: Vec<i32>, queries: Vec<Vec<i32>>) -> Vec<i32> {
         let mut rs = Vec::new();
 
         for q in &queries {
-            let mut set = BTreeSet::new();
-            for i in q[0]..=q[1] {
-                set.insert(nums.get(i as usize).unwrap());
+            let (s, e) = (q[0] as usize, q[1] as usize);
+            let mut a = Vec::new();
+            for i in s..=e {
+                match a.binary_search(&nums[i]) {
+                    Ok(_) => continue,
+                    Err(e) => a.insert(e, nums[i]),
+                }
             }
-            if set.len() == 0 {
+            if a.len() < 2 {
                 rs.push(-1);
                 continue;
             }
             let mut min = 101;
-            let mut prev = 200;
-            for x in set {
-                let a = (*x - prev).abs();
-                if a == 0 {
-                    continue;
+            for i in 0..a.len()-1 {
+                let t = a[i+1] - a[i];
+                if t < min {
+                    min = t;
                 }
-                if min > a {
-                    min = a;
-                }
-                prev = *x;
             }
-            min = if min == 101 {
-                -1
-            } else {
-                min
-            };
             rs.push(min);
         }
 
